16bc56f209a757b9306d3cd1b99dfee96e4336dc
cckn <cckn.dev@gmail.com>
2022-11-20 18:01:57 +0900

    기본 예시

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-interface IMyInterface {
-	myProperty: string
+let shopping_cart = []
+let shopping_cart_total = 0
+
+function add_item_to_cart(name, price) {
+	shopping_cart.push({name, price})
+	shopping_cart_total += price
 }
 
-console.log('Hello World!2')
+function calc_cart_total() {
+	shopping_cart_total = 0
+	for (let i = 0; i < shopping_cart.length; i++) {
+		const item = shopping_cart[i]
+		shopping_cart_total += shopping_cart[i]
+	}
+	set_cart_total_dom()
+}
+
+function set_cart_total_dom() {
+	console.log(`Total: ${shopping_cart_total}`)
+}
